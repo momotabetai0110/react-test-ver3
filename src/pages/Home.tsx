5fdadeb2bd065f26cb4c833c371417aeb96cf641
momotabetai0110 <momotabetai0110@gmail.com>
2026-02-14 14:44:02 +0900

    タスクの追加と取得を追加

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,75 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import { useState } from "react";
+export default function Home() {
+  const [taskName, setTaskName] = useState<string>("");
+  const [taskList, setTaskList] = useState<string[]>([]);
 
-export default function Home(){
-    return(
-        <div>こんにちは</div>
-    )
+  //cookieが存在するかチェック
+  function hasCookieTask(): string[] {
+    const match = document.cookie.match(
+      new RegExp("(?:^|; )" + encodeURIComponent("task") + "=([^;]*)"),
+    );
+    if (!match) return [];
+    const decoded = decodeURIComponent(match[1]);
+    const parsed = JSON.parse(decoded);
+    return parsed;
+  }
+
+  //cookieにタスクを保存
+  function setCookieTask(name: string, value: string) {
+    //保存用配列にタスクをプッシュ
+    const taskList = hasCookieTask();
+    taskList.push(value);
+    //配列を文字列に変換
+    const encodedValue = encodeURIComponent(JSON.stringify(taskList));
+
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7);
+    const encodedName = encodeURIComponent(name);
+
+    document.cookie = `${encodedName}=${encodedValue}; path=/; expires=${expires.toUTCString()}`;
+    setTaskList(taskList);
+  }
+
+//   //タスクを取得
+//   function getCookieTask() {
+//     const match = document.cookie.match(
+//       new RegExp("(?:^|; )" + encodeURIComponent("task") + "=([^;]*)"),
+//     );
+//     if (!match) return [];
+//     const decoded = decodeURIComponent(match[1]);
+//     const data = JSON.parse(decoded);
+//     setTaskList(data);
+//   }
+
+  return (
+    <div>
+      <div>こんにちは</div>
+      {taskList !== null &&
+        taskList.map((task, index) => {
+          return <div key={index}>{task}</div>;
+        })}
+      <form
+        onSubmit={(e) => {
+          setCookieTask("task", taskName);
+          e.preventDefault();
+        }}
+      >
+        <input
+          type="string"
+          value={taskName}
+          onChange={(e) => setTaskName(e.target.value)}
+        />
+        <button className="btn btn-success" type="submit">
+          タスク追加
+        </button>
+      </form>
+      <button
+        className="btn btn-primary"
+        onClick={() => setCookieTask("task", "仮タスク")}
+      >
+        保存
+      </button>
+    </div>
+  );
 }
