0ca71eb2260427b62534bff649133153510bc7c8
momotabetai0110 <momotabetai0110@gmail.com>
2026-02-14 21:09:02 +0900

    タスクの削除

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 // import { Link } from "react-router-dom";
 import { useState } from "react";
+
 export default function Home() {
   const [taskName, setTaskName] = useState<string>("");
   const [taskList, setTaskList] = useState<string[]>([]);
@@ -15,43 +16,60 @@ export default function Home() {
     return parsed;
   }
 
-  //cookieにタスクを保存
-  function setCookieTask(name: string, value: string) {
+  //cookieにタスクを追加
+  function setCookieTask(value: string) {
     //保存用配列にタスクをプッシュ
     const taskList = hasCookieTask();
     taskList.push(value);
     //配列を文字列に変換
     const encodedValue = encodeURIComponent(JSON.stringify(taskList));
-
-    const expires = new Date();
-    expires.setDate(expires.getDate() + 7);
-    const encodedName = encodeURIComponent(name);
-
-    document.cookie = `${encodedName}=${encodedValue}; path=/; expires=${expires.toUTCString()}`;
+    saveCookieTask(encodedValue);
     setTaskList(taskList);
   }
 
-//   //タスクを取得
-//   function getCookieTask() {
-//     const match = document.cookie.match(
-//       new RegExp("(?:^|; )" + encodeURIComponent("task") + "=([^;]*)"),
-//     );
-//     if (!match) return [];
-//     const decoded = decodeURIComponent(match[1]);
-//     const data = JSON.parse(decoded);
-//     setTaskList(data);
-//   }
+  //クッキーにタスクを保存
+  function saveCookieTask(value: string) {
+    //cookieの保存期限を定義
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7);
+    //cookieに保存するnameを定義
+    const name = encodeURIComponent("task");
+    //cookieに値を保存
+    document.cookie = `${name}=${value}; path=/; expires=${expires.toUTCString()}`;
+  }
+
+  //cookieからタスクを削除
+  function deleteCookieTask(target: number) {
+    //指定したタスクを削除
+    const taskList = hasCookieTask();
+    taskList.splice(target, 1);
+    //削除した配列でcookieを上書き
+    const encodedValue = encodeURIComponent(JSON.stringify(taskList));
+    saveCookieTask(encodedValue);
+    //画面向けに再取得
+    setTaskList(taskList);
+  }
 
   return (
     <div>
       <div>こんにちは</div>
       {taskList !== null &&
         taskList.map((task, index) => {
-          return <div key={index}>{task}</div>;
+          return (
+            <div>
+              <div key={index}>{task}</div>
+              <button
+                className="btn btn-danger"
+                onClick={() => deleteCookieTask(index)}
+              >
+                削除
+              </button>
+            </div>
+          );
         })}
       <form
         onSubmit={(e) => {
-          setCookieTask("task", taskName);
+          setCookieTask(taskName);
           e.preventDefault();
         }}
       >
@@ -66,7 +84,7 @@ export default function Home() {
       </form>
       <button
         className="btn btn-primary"
-        onClick={() => setCookieTask("task", "仮タスク")}
+        onClick={() => setCookieTask("仮タスク")}
       >
         保存
       </button>
